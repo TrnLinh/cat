82b9dda6abfcf8076ea673ac872a16ccd24178eb
TrnLinh <81613310+TrnLinh@users.noreply.github.com>
2023-06-01 22:34:43 +0700

    Add files via upload

---
+++ js/darkmode.js
@@ -0,0 +1,17 @@
+//light mode toggle
+checkbox.addEventListener("change", function () {
+  if (this.checked) {
+    trans();
+    document.documentElement.setAttribute("data-theme", "dark");
+  } else {
+    trans();
+    document.documentElement.setAttribute("data-theme", "light");
+  }
+});
+
+let trans = () => {
+  document.documentElement.classList.add("transition");
+  window.setTimeout(() => {
+    document.documentElement.classList.remove("transition");
+  }, 1000);
+};
---
+++ js/script.js
@@ -0,0 +1,48 @@
+//nav bar
+
+document
+  .querySelector(".hamburger-menu")
+  .addEventListener("click", function () {
+    document.querySelector(".bar").classList.toggle("animate");
+    var mobileNav = document.querySelector(".mobile-nav");
+    mobileNav.classList.toggle("show");
+  });
+
+var checkbox = document.querySelector("input[name=theme]");
+
+//loading screen
+var loader = document.getElementById("loadingscreen");
+
+window.addEventListener("load", function () {
+  loader.style.opacity = "0";
+  timeLoading();
+  trans();
+});
+
+let timeLoading = () => {
+  window.setTimeout(() => {
+    loader.style.opacity = "0";
+  }, 5100);
+};
+
+let trans = () => {
+  document.documentElement.classList.add("transition");
+  window.setTimeout(() => {
+    document.documentElement.classList.remove("transition");
+    loader.style.display = "none";
+  }, 5300);
+};
+//paralax
+
+window.addEventListener("scroll", function (e) {
+  const target = this.document.querySelectorAll(".scroll");
+
+  var index = 0;
+  var length = target.length;
+
+  for (index; index < length; index++) {
+    var pos = window.pageYOffset * target[index].dataset.rate;
+
+    target[index].style.transform = "translate3d(0px," + pos + "px, 0px)";
+  }
+});
